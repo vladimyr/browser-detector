f038fb64244be58fd4cc273c544aa066ae9c89e5
Dario Vladovic <d.vladimyr@gmail.com>
2017-03-10 18:40:36 +0100

    :tada: Introducing upload to sprunge feature
    - adding form for sprunge.us (online pastebin) uploads

--- index.js
+++ index.js
@@ -12,13 +12,16 @@ let parser = new UAParser();
 let $dump = template('ua-parser-js', version, parser.getResult());
 $output.appendChild($dump);
 
-function template(name, version, object) {
-  let $dump = h('.object-dump');
-  let inspector = new ObjectInspector({ element: $dump, collapsed: false });
-  inspector.view(object);
+function template(name, version, obj) {
+  let $objectDump = h('.object-dump');
+  let inspector = new ObjectInspector({ element: $objectDump, collapsed: false });
+  inspector.view(obj);
 
   return h(`.inspector .inspector-${ name }`,
     h('a', { href: packageUrl(name), target: '_blank' },
       h('span.title', `${ name }@${ version }`)),
-    $dump);
+    $objectDump,
+    h('form.upload-form', { action: 'http://sprunge.us', method: 'POST', 'accept-charset': 'UTF-8' },
+      h('textarea', { name: 'sprunge', value: JSON.stringify(obj, null, 2) }),
+      h('input.btn-upload', { type: 'submit', value: 'Upload to sprunge.us' })));
 }
