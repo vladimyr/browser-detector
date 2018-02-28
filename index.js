71c0294b7e1db2d0035c9c1aa26eaea5101ed723
Dario Vladovic <d.vladimyr@gmail.com>
2018-02-28 01:34:34 +0100

    Bumping dependencies :arrow_up:

--- index.js
+++ index.js
@@ -5,21 +5,21 @@ const UAParser = require('ua-parser-js');
 const version = require('ua-parser-js/package.json').version;
 const ObjectInspector = require('Inspector-JSON');
 
-const packageUrl = name => `https://www.npmjs.com/package/${ name }`;
+const packageUrl = name => `https://www.npmjs.com/package/${name}`;
 
-let $output = document.querySelector('.inspectors');
-let parser = new UAParser();
-let $dump = template('ua-parser-js', version, parser.getResult());
+const $output = document.querySelector('.inspectors');
+const parser = new UAParser();
+const $dump = template('ua-parser-js', version, parser.getResult());
 $output.appendChild($dump);
 
 function template(name, version, obj) {
-  let $objectDump = h('.object-dump');
-  let inspector = new ObjectInspector({ element: $objectDump, collapsed: false });
+  const $objectDump = h('.object-dump');
+  const inspector = new ObjectInspector({ element: $objectDump, collapsed: false });
   inspector.view(obj);
 
-  return h(`.inspector .inspector-${ name }`,
+  return h(`.inspector .inspector-${name}`,
     h('a', { href: packageUrl(name), target: '_blank' },
-      h('span.title', `${ name }@${ version }`)),
+      h('span.title', `${name}@${version}`)),
     $objectDump,
     h('form.upload-form', { action: 'http://sprunge.us', method: 'POST', 'accept-charset': 'UTF-8' },
       h('textarea', { name: 'sprunge', value: JSON.stringify(obj, null, 2) }),
