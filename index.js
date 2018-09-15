e4d0e4017b049daa8da89cc3110242f26ec125bd
Dario Vladovic <d.vladimyr@gmail.com>
2018-09-15 12:22:42 +0200

    Migrate to parcel bundler :zap:
    - swapping browserify with parcel bundler for improved build times
      and easier development :tada:
    - migrating from jshint to eslint (eslint-config-semistandard)
    - adding CI/CD using Travis
    - keeping deeps green using renovatebot

--- index.js
+++ index.js
@@ -1,35 +1,44 @@
 'use strict';
 
-const h = require('hyperscript');
-const UAParser = require('ua-parser-js');
-const version = require('ua-parser-js/package.json').version;
-const ObjectInspector = require('Inspector-JSON');
+import h from 'hyperscript';
+import ObjectInspector from 'Inspector-JSON';
+import UAParser, { VERSION as version } from 'ua-parser-js';
+const urlParser = document.createElement('a');
 
-const packageUrl = name => `https://www.npmjs.com/package/${name}`;
+const domain = url => {
+  urlParser.href = url;
+  return urlParser.hostname;
+};
+const packageUrl = name => `https://npm.im/${name}`;
 
-const $output = document.querySelector('.inspectors');
 const parser = new UAParser();
-const $dump = template('ua-parser-js', version, parser.getResult());
-$output.appendChild($dump);
+const $output = document.querySelector('.inspectors');
+const $fragment = template('ua-parser-js', version, parser.getResult());
+$output.appendChild($fragment);
 
 function template(name, version, obj) {
-  const $objectDump = h('.object-dump');
-  const inspector = new ObjectInspector({ element: $objectDump, collapsed: false });
-  inspector.view(obj);
-
-  const value = JSON.stringify(obj, null, 2);
+  const json = JSON.stringify(obj, null, 2);
   return h(`.inspector .inspector-${name}`,
     h('a', { href: packageUrl(name), target: '_blank' },
       h('span.title', `${name}@${version}`)),
-    $objectDump,
+    jsonInspector(obj, { collapsed: false }),
     h('.upload',
-      // ix.io upload button
-      h('form.upload-form', { action: 'http://ix.io', method: 'POST', 'accept-charset': 'UTF-8' },
-        h('textarea', { name: 'f:1', value }),
-        h('input.btn-upload', { type: 'submit', value: 'Upload to ix.io' })),
-      // sprunge upload button
-      h('form.upload-form', { action: 'http://sprunge.us', method: 'POST', 'accept-charset': 'UTF-8' },
-        h('textarea', { name: 'sprunge', value }),
-        h('input.btn-upload', { type: 'submit', value: 'Upload to sprunge.us' })))
-    )
+      uploadButton('http://ix.io', { 'f:1': json }),
+      uploadButton('http://sprunge.us', { sprunge: json })));
+}
+
+function jsonInspector(obj, options) {
+  const element = h('.object-dump');
+  options = Object.assign({ element }, options);
+  const inspector = new ObjectInspector(options);
+  inspector.view(obj);
+  return element;
+}
+
+function uploadButton(action, payload, label = `Upload to ${domain(action)}`) {
+  const [name] = Object.keys(payload);
+  const value = payload[name];
+  return h('form.upload-form', { action, method: 'POST', 'accept-charset': 'UTF-8' },
+    h('textarea', { name, value }),
+    h('input.btn-upload', { type: 'submit', value: label }));
 }
