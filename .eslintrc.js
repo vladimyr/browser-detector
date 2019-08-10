d268060778363e1772f8bdff2c64d79f60f9c749
Dario Vladovic <d.vladimyr@gmail.com>
2019-08-10 21:58:40 +0200

    Cleanup ESLint config :lipstick:

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,6 @@
 module.exports = {
   extends: 'semistandard',
   rules: {
-    'prefer-const': 'error',
     'space-before-function-paren': ['error', {
       anonymous: 'always',
       named: 'never'
